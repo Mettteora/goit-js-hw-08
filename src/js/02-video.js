e3235e211b987678c59a9756c71b5875b2389df4
Mettteora <mrhens2013@gmail.com>
2023-04-01 18:46:56 +0200

    123

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player.Player(iframe);
+
+// player.on('timeupdate', function (data) {
+//   const currentTime = data.seconds;
+//   localStorage.setItem('videoplayer-current-time', currentTime);
+//   console.log('played the video!');
+// });
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+player.on('timeupdate', function (data) {
+  const currentTime = data.seconds;
+  localStorage.setItem('videoplayer-current-time', currentTime);
+});
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+if (currentTime !== null) {
+  player.setCurrentTime(currentTime);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,29 @@
+const info = {
+  email: ``,
+  meassage: ``,
+};
+const inputMessage = document.querySelector(
+  '.feedback-form textarea[name="message"]'
+);
+const inputEmail = document.querySelector('.feedback-form input[name="email"]');
+const btn = document.querySelector('.feedback-form button[type="submit"]');
+const form = document.querySelector(`feedback-form`);
+console.log(inputMessage);
 
+inputMessage.addEventListener(`input`, checkMessage);
+
+inputEmail.addEventListener(`input`, checkEmail);
+
+function checkMessage(e) {
+  info.meassage = e.currentTarget.value;
+  console.log(info);
+}
+
+function checkEmail(e) {
+  info.email = e.currentTarget.value;
+  console.log(info);
+}
+
+if (!info.email === ``) {
+  console.log(`тут не пусто `);
+}
