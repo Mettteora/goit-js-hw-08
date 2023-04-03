e456ee1b50e6c5bcf79966facf9ffd52ed7738f9
Mettteora <mrhens2013@gmail.com>
2023-04-03 21:02:42 +0200

    123

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,23 +1,29 @@
-// const Player = require(`@vimeo/player`);
-
-// // import Player from '@vimeo/player';
+//1. Потрібно зробити імпорт модуля vimeo
 import Player from '@vimeo/player';
 
-import { throttle } from 'lodash';
+//2. Потрібно додати до плеера метод throttle
+import throttle from 'lodash.throttle';
 
+//3. Забираємо плеер з html
 const iframe = document.querySelector('iframe');
-const player = new Player.Player(iframe);
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+//4. Активуємо плеер vimeo
+const player = new Player(iframe);
 
-player.on('timeupdate', function (data) {
-  const currentTime = data.seconds;
-  localStorage.setItem('videoplayer-current-time', currentTime);
-});
+//5. В плеер додаємо слухач подій завдяки методу 'on', далі як параметр передаємо подію 'timeupdate'
+player.on('timeupdate', throttle(saveTime, 1000));
 
-const currentTime = localStorage.getItem('videoplayer-current-time');
-if (currentTime !== null) {
-  player.setCurrentTime(currentTime);
+//6. За допомогою ф-ції 'saveTime' додаємо ключ videoplayer-current-time
+function saveTime(event) {
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(event.seconds)
+  );
+  console.log(set);
 }
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
+
+//7. Необхідно дадати до плеера щоб було позначення з якого моменту потрібно починати відео.
+const currentTime = localStorage.getItem('videoplayer-current-time');
+console.log(currentTime);
